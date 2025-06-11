fc1f6a33132898794bf122204a5f7a6ecddae8d7
Нурматов Умархон Акмалович <uanurmatov@edu.hse.ru>
2025-06-11 13:27:33 +0300

    Cleaned controller tb from unneccesary functions

--- source/controller/tb/tb_controller.sv
+++ source/controller/tb/tb_controller.sv
@@ -1,51 +1,37 @@
 `timescale 1ns / 1ps
 module tb_controller;
-    /* Test bench specific parameters */
-        localparam baud_rate = 9600;
-        localparam sys_clk_freq = 50000000;
+    localparam baud_rate = 9600;
+    localparam sys_clk_freq = 50000000;
 
-    /* Clock simulation */
-        localparam CLK_PERIOD = 10;         // in nanoseconds
-    
-    /* General shortcuts */
-        localparam T = 1'b1;
-        localparam F = 1'b0;
-        
-//** SIGNAL DECLARATIONS **************************************
- 
-    /* UUT signals */
-        reg clk;
-        reg rstn;
-        reg rx;
-        wire tx;
-        wire ready;
-       
-//** Clock ****************************************************     
- 
+    localparam CLK_PERIOD = 10;  //ns
+
+    localparam T = 1'b1;
+    localparam F = 1'b0;
+
+    reg  clk;
+    reg  rstn;
+    reg  rx;
+    wire tx;
+    wire ready;
+
     initial begin
         clk <= F;
         forever begin
-            #(CLK_PERIOD / 2); clk <= ~ clk;
+            #(CLK_PERIOD / 2);
+            clk <= ~clk;
         end
     end
 
-//** UUT Tests ************************************************ 
-
     initial begin
-    
         $dumpvars();
         initial_conditions();
-        reset_UUT();
+        reset_DUT();
 
-        // FETCH_WEIGHTS
+        //////////// FETCH WEIGHTS ////////////
         send_byte(8'h55);
-        send_byte(8'h02); 
+        send_byte(8'h02);
 
-        // WEIGHTS
-        send_byte(8'h55);
-        send_byte(8'h01);
-        send_byte(8'h11);
-
+        //////////// SEND WEIGHTS  ////////////
         send_byte(8'h55);
         send_byte(8'h01);
         send_byte(8'h11);
@@ -58,12 +44,15 @@ module tb_controller;
         send_byte(8'h01);
         send_byte(8'h11);
 
-        // LOAD WEIGHTS
+        send_byte(8'h55);
+        send_byte(8'h01);
+        send_byte(8'h11);
+
+        //////////// LOAD WEIGHTS  ////////////
         send_byte(8'h55);
         send_byte(8'h03);
 
-
-        // DATA
+        //////////// LOAD DATA     ////////////
         send_byte(8'h55);
         send_byte(8'h01);
         send_byte(8'h11);
@@ -80,78 +69,61 @@ module tb_controller;
         send_byte(8'h01);
         send_byte(8'h11);
 
-
-        // START COMP
+        ////////// START COMP      ///////////
         send_byte(8'h55);
         send_byte(8'h04);
 
-        wait(ready == 1'b1);
-        
+        wait (ready == 1'b1);
+
         $finish;
     end
-    
-    
-//** Tasks **************************************************** 
-    
-    task initial_conditions(); begin
-        repeat(5) @(posedge clk)
-        rstn = T; 
-        rx = T;
-    end endtask  
 
-
-    task reset_UUT(); begin
-        @(posedge clk)
-        rstn = F;
-        @(posedge clk)
-        rstn = T;
-    end endtask  
-
-
-    task delay(input integer N); begin
-        repeat(N) @(posedge clk);
-    end endtask  
-    
-    
-    task send_byte (input reg[7:0] C); 
-        reg [7:0] shift_reg;
-    begin
-        shift_reg = C;
-        @(posedge clk)
-        rx = F;                         // send start bit
-        delay(sys_clk_freq / baud_rate);
-            
-        for(int i = 0; i < 8; i++) begin    // send serial data LSB first
-            rx = shift_reg[0];
-            shift_reg = shift_reg >> 1;
-            delay(sys_clk_freq / baud_rate);
+    task initial_conditions();
+        begin
+            repeat (5) @(posedge clk) rstn = T;
+            rx = T;
         end
-        
-        rx = T;                         // send stop bit
-        delay(sys_clk_freq / baud_rate);
-        
-    end endtask
-    
-    task send_int (input reg[31:0] i); begin
-        send_byte(8'h55); // start byte
-        send_byte(8'h01); // cmd
-        send_byte(i[7:0]); // data[0][0]
-        send_byte(i[15:8]); // data[0][1]
-        send_byte(i[23:16]); // data[0][2]
-        send_byte(i[31:24]); // data[0][3]
-    end endtask
+    endtask
+
+    task reset_DUT();
+        begin
+            @(posedge clk) rstn = F;
+            @(posedge clk) rstn = T;
+        end
+    endtask
 
 
-//** INSTANTIATE THE UNIT UNDER TEST(UUT)**********************
+    task delay(input integer N);
+        begin
+            repeat (N) @(posedge clk);
+        end
+    endtask
 
-    controller 
-    controller_inst (
+    task send_byte(input reg [7:0] C);
+        reg [7:0] shift_reg;
+        begin
+            shift_reg = C;
+            @(posedge clk) rx = F;  // send start bit
+            delay(sys_clk_freq / baud_rate);
+
+            for (int i = 0; i < 8; i++) begin  // send serial data LSB first
+                rx = shift_reg[0];
+                shift_reg = shift_reg >> 1;
+                delay(sys_clk_freq / baud_rate);
+            end
+
+            rx = T;  // send stop bit
+            delay(sys_clk_freq / baud_rate);
+
+        end
+    endtask
+
+    controller dut (
         .clk(clk),
         .rstn(rstn),
         .rx(rx),
         .tx(tx),
         .ready(ready)
     );
-
 
 endmodule
