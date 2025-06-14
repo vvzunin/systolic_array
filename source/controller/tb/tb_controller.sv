89e6d749e1df8804bfed90a4a9e653cd9dba5e92
Нурматов Умархон Акмалович <uanurmatov@edu.hse.ru>
2025-06-14 23:16:31 +0300

    Modified and ehnaced tb

--- source/controller/tb/tb_controller.sv
+++ source/controller/tb/tb_controller.sv
@@ -1,12 +1,22 @@
 `timescale 1ns / 1ps
+`include "../uart_parser.svh"
 module tb_controller;
-    localparam baud_rate = 9600;
-    localparam sys_clk_freq = 50000000;
-
     localparam CLK_PERIOD = 10;  //ns
 
     localparam T = 1'b1;
     localparam F = 1'b0;
+
+    parameter DATA_WIDTH = 32, 
+              MAX_LEN    = 256,
+              ARRAY_W    = 10, 
+              ARRAY_L    = 10, 
+              ARRAY_A_W  = 4,  
+              ARRAY_A_L  = 3,  
+              ARRAY_W_W  = 3, 
+              ARRAY_W_L  = 4,  
+              BAUD_RATE  = 115200,
+              W_DIGIT    = 4,
+              CLK_FREQ   = 1_000_000_000 / CLK_PERIOD;
 
     reg  clk;
     reg  rstn;
@@ -28,50 +38,28 @@ module tb_controller;
         reset_DUT();
 
         //////////// FETCH WEIGHTS ////////////
-        send_byte(8'h55);
-        send_byte(8'h02);
+        send_byte(`START_BYTE);
+        send_byte(`CMD_FETCH_WEIGHTS);
 
         //////////// SEND WEIGHTS  ////////////
-        send_byte(8'h55);
-        send_byte(8'h01);
-        send_byte(8'h11);
+        repeat (ARRAY_W_L * ARRAY_W_W) send_int32(32'hffff_ffff);
 
-        send_byte(8'h55);
-        send_byte(8'h01);
-        send_byte(8'h11);
-
-        send_byte(8'h55);
-        send_byte(8'h01);
-        send_byte(8'h11);
-
-        send_byte(8'h55);
-        send_byte(8'h01);
-        send_byte(8'h11);
 
         //////////// LOAD WEIGHTS  ////////////
-        send_byte(8'h55);
-        send_byte(8'h03);
+        send_byte(`START_BYTE);
+        send_byte(`CMD_LOAD_WEIGHTS);
+
+
+        //////////// FETCH DATA    ////////////
+        send_byte(`START_BYTE);
+        send_byte(`CMD_FETCH_DATA);
 
         //////////// LOAD DATA     ////////////
-        send_byte(8'h55);
-        send_byte(8'h01);
-        send_byte(8'h11);
-
-        send_byte(8'h55);
-        send_byte(8'h01);
-        send_byte(8'h11);
-
-        send_byte(8'h55);
-        send_byte(8'h01);
-        send_byte(8'h11);
-
-        send_byte(8'h55);
-        send_byte(8'h01);
-        send_byte(8'h11);
+        repeat (ARRAY_A_L * ARRAY_A_W) send_int32(32'hffff_ffff);
 
         ////////// START COMP      ///////////
-        send_byte(8'h55);
-        send_byte(8'h04);
+        send_byte(`START_BYTE);
+        send_byte(`CMD_START_COMP);
 
         wait (ready == 1'b1);
 
@@ -104,21 +92,39 @@ module tb_controller;
         begin
             shift_reg = C;
             @(posedge clk) rx = F;  // send start bit
-            delay(sys_clk_freq / baud_rate);
+            delay(CLK_FREQ / BAUD_RATE);
 
             for (int i = 0; i < 8; i++) begin  // send serial data LSB first
                 rx = shift_reg[0];
                 shift_reg = shift_reg >> 1;
-                delay(sys_clk_freq / baud_rate);
+                delay(CLK_FREQ / BAUD_RATE);
             end
 
             rx = T;  // send stop bit
-            delay(sys_clk_freq / baud_rate);
+            delay(CLK_FREQ / BAUD_RATE);
 
         end
     endtask
 
-    controller dut (
+    task send_int32(input reg [31:0] C);
+        for (int i = 0; i < 4; i++) begin
+            send_byte(`START_BYTE);
+            send_byte(`CMD_DATA);
+            send_byte(C[i*8+:8]);
+        end
+    endtask
+
+    controller #(
+        .DATA_WIDTH(DATA_WIDTH),
+        .ARRAY_W(ARRAY_W),
+        .ARRAY_L(ARRAY_L),
+        .ARRAY_W_W(ARRAY_W_W),
+        .ARRAY_W_L(ARRAY_W_L),
+        .ARRAY_A_W(ARRAY_A_W),
+        .ARRAY_A_L(ARRAY_A_L),
+        .CLK_FREQ(CLK_FREQ),
+        .BAUD_RATE(BAUD_RATE)
+    ) dut (
         .clk(clk),
         .rstn(rstn),
         .rx(rx),
